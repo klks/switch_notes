a6bb294126504b77d666c45946575ac70fdc5f94
klks <klks84@gmail.com>
2018-06-22 20:20:03 +0800

    Moving code around

---
+++ fixup_memory/const_pmc.py
@@ -0,0 +1,23 @@
+#https://github.com/Atmosphere-NX/Atmosphere/blob/master/fusee/fusee-primary/src/hwinit/pmc.h
+pmc_values = {
+"APBDEV_PMC_PWRGATE_TOGGLE"         : 0x54501030,
+"APBDEV_PMC_PWRGATE_STATUS"         : 0x54501038,
+"APBDEV_PMC_NO_IOPOWER"             : 0x54501044,
+"APBDEV_PMC_SCRATCH20"              : 0x545010A0,
+"APBDEV_PMC_DDR_PWR"                : 0x545010E8,
+"APBDEV_PMC_CRYPTO_OP"              : 0x545010F4,
+"APBDEV_PMC_OSC_EDPD_OVER"          : 0x545011A4,
+"APBDEV_PMC_IO_DPD_REQ"             : 0x545011B8,
+"APBDEV_PMC_IO_DPD2_REQ"            : 0x545011C0,
+"APBDEV_PMC_VDDP_SEL"               : 0x545011CC,
+"APBDEV_PMC_TSC_MULT"               : 0x545012B4,
+"APBDEV_PMC_REG_SHORT"              : 0x545012CC,
+"APBDEV_PMC_WEAK_BIAS"              : 0x545012C8,
+"APBDEV_PMC_SECURE_SCRATCH21"       : 0x54501334,
+"APBDEV_PMC_CNTRL2"                 : 0x54501440,
+"APBDEV_PMC_IO_DPD4_REQ"            : 0x54501464,
+"APBDEV_PMC_DDR_CNTRL"              : 0x545014E4,
+"APBDEV_PMC_SCRATCH188"             : 0x54501810,
+"APBDEV_PMC_SCRATCH190"             : 0x54501818,
+"APBDEV_PMC_SCRATCH200"             : 0x54501840
+}
