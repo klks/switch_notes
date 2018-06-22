aa651a12984eb7829581f1197a4a4e341cd9d827
klks <klks84@gmail.com>
2018-06-23 00:28:57 +0800

    more rebasing

--- fixup_memory/const_exec_vec.py
+++ fixup_memory/const_exec_vec.py
@@ -1,4 +1,4 @@
 exec_vec_values = {
-"base": 0x6000F000,
-"EXEC_VEC_0x100":   0x100
+"base"                      : 0x6000F000,
+"EXEC_VEC_0x100"            : 0x100
 }
--- fixup_memory/const_flow_ctrl.py
+++ fixup_memory/const_flow_ctrl.py
@@ -1,4 +1,5 @@
 flow_ctrl_values = {
-"FLOW_CTLR_RAM_REPAIR"              : 0x60007040,
-"FLOW_CTLR_BPMP_CLUSTER_CONTROL"    : 0x60007098,
+"base"                              : 0x60007000,
+"FLOW_CTLR_RAM_REPAIR"              : 0x40,
+"FLOW_CTLR_BPMP_CLUSTER_CONTROL"    : 0x98,
 }
